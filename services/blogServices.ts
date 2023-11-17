b46ce2919c16e3dc04828ac4b41c72cfc027ebc7
Gregorio Cardona <dani12ca12@gmail.com>
2023-11-17 10:51:36 -0500

    version1.0

--- services/blogServices.ts
+++ services/blogServices.ts
@@ -1,41 +1,43 @@
 import axios from 'axios'
 // import { PostType} from '@/types/types'
 
+ const API_URL = process.env.NEXTAUTH_URL
+
 //obtener todos las categorias
 const getData = async()=>{
-    const res = await axios.get(`${process.env.NEXTAUTH_URL}/api/category`)
+    const res = await axios.get(`${API_URL}/api/category`)
     return res.data
 }
 
 //crear un post
 const createPost = async(body:any)=>{
-    const res = await axios.post(`http://localhost:3000/api/posts`, body)
+    const res = await axios.post(`${API_URL}/api/posts`, body)
     return res.data
 }
 
 //obtener todos los posts
 const getPosts = async(page:any, cat:any)=>{
-    const res = await axios.get(`http://localhost:3000/api/posts?page=${page || 1}&cat=${cat || ""}`)
+    const res = await axios.get(`${API_URL}/api/posts?page=${page || 1}&cat=${cat || ""}`)
     return res.data
 }
 
 const getSinglePost = async(slug:string)=>{
     // console.log(slug)
-    const res = await axios.get(`http://localhost:3000/api/posts/${slug}`)
+    const res = await axios.get(`${API_URL}/api/posts/${slug}`)
     // console.log("🚀 ~ file: blogServices.tsx:25 ~ getSinglePost ~ res:", res)
     return res.data
 }
 
 const getComments = async(slug:string)=>{
     // console.log(slug)
-    const res = await axios.get(`http://localhost:3000/api/comments?postSlug=${slug}`)
+    const res = await axios.get(`${API_URL}/api/comments?postSlug=${slug}`)
     console.log("🚀 ~ file: blogServices.tsx:25 ~ getSinglePost ~ res:", res)    
     return res.data
 }
 
 const createComment = async(body: any)=>{
     // console.log(body)
-    const res = await axios.post(`http://localhost:3000/api/comments`, body)
+    const res = await axios.post(`${API_URL}/api/comments`, body)
     return res.data
 }
 
